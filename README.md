3987a12f460b21a3b5fe0921acd5f3d7a3ec8ec9
eight <eight04@gmail.com>
2018-06-27 23:53:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,126 @@
+inline-js-default-transforms
+============================
+
+[![Build Status](https://travis-ci.org/eight04/inline-js-default-transforms.svg?branch=master)](https://travis-ci.org/eight04/inline-js-default-transforms)
+[![codecov](https://codecov.io/gh/eight04/inline-js-default-transforms/branch/master/graph/badge.svg)](https://codecov.io/gh/eight04/inline-js-default-transforms)
+[![install size](https://packagephobia.now.sh/badge?p=inline-js-default-transforms)](https://packagephobia.now.sh/result?p=inline-js-default-transforms)
+
+This repository contains builtin transformers for [inline-js](https://github.com/eight04/inline-js)
+
+Installation
+------------
+```js
+npm install inline-js-default-transforms
+```
+
+Usage
+-----
+```js
+const {createInliner} = require("inline-js-core");
+const {TRANSFORMS} = require("inline-js-default-transforms");
+
+const inliner = createInliner();
+TRANSFORMS.forEach(inliner.transformer.add);
+```
+
+TRANSFORMS
+----------
+
+### cssmin
+Minify css content.
+
+### dataurl
+Convert the content into data URL.
+
+The transformer would determine the mimetype from the filename:
+```js
+// data:text/css;charset=utf8;base64,...
+$inline("mystyle.css|dataurl")
+
+// data:image/png;base64,...
+$inline("myimage.png|dataurl")
+```
+Or you can pass the mimetype manually:
+```js
+$inline("somefile.txt|dataurl:text/css")
+```
+Specify charset (default to `utf8` for text files):
+```js
+$inline("somefile.txt|dataurl:text/css,utf8")
+```
+
+### docstring
+Extract docstring (i.e. the top-most template literal) from the content.
+
+### eval
+Evaluate JavaScript expression. You can access the content with `$0`.
+```js
+var version = $inline("./package.json|eval:JSON.parse($0).version|stringify");
+```
+
+### indent
+Indent the string according to the indent of the current line.
+
+*entry.js*
+```js
+function test() {
+  $inline("foo.js|indent");
+}
+```
+*foo.js*
+```js
+console.log("foo");
+console.log("bar");
+```
+`inlinejs entry.js` result:
+```js
+function test() {
+  console.log("foo");
+  console.log("bar");
+}
+```
+
+### markdown
+Wrap content with markdown codeblock, code, or quote.
+````js
+// a.txt
+some text
+
+// $inline("a.txt|markdown:codeblock")
+```
+some text
+```
+
+// $inline("a.txt|markdown:code")
+`some text`
+
+// $inline("a.txt|markdown:quote")
+> sometext
+````
+
+### parse
+`JSON.parse` the content. You can access properties by specifying key name.
+```js
+var version = $inline("./package.json|parse:version"),
+  nestedProp = $inline("./package.json|parse:nested,prop");
+```
+
+### string
+
+If the content is a buffer, convert it into a utf8 string. Otherwise do nothing.
+
+### stringify
+`JSON.stringify` the content. Useful to include text content into JavaScript code:
+```js
+var myCssString = $inline("./style.css|cssmin|stringify");
+```
+
+### trim
+`String.prototype.trim` the content.
+
+Changelog
+---------
+
+* 0.1.0 (Jun 27, 2018)
+
+    - First release.
---
+++ test/test.js
@@ -0,0 +1,194 @@
+/* eslint-env mocha */
+const assert = require("assert");
+const dataurl = require("dataurl");
+const {createTransformer} = require("inline-js-core/lib/transformer");
+const {TRANSFORMS} = require("..");
+
+describe("transforms", () => {
+  const transformer = createTransformer();
+  TRANSFORMS.forEach(transformer.add);
+	
+  function prepare(baseOptions) {
+    return options => {
+      const {
+        name,
+        content,
+        args = [],
+        expect,
+        source,
+        error,
+        ctx = {inlineTarget: source}
+      } = Object.assign({}, baseOptions, options);
+      return transformer.transform(ctx, content, [{name, args}])
+        .then(
+          result => assert.equal(result, expect),
+          err => {
+            if (!error) {
+              throw err;
+            }
+          }
+        );
+    };
+  }
+	
+	it("cssmin", () => {
+    const test = prepare({
+      name: "cssmin",
+      content: 'body {\n  color: #000000;\n}\n',
+      expect: "body{color:#000}"
+    });
+    return test();
+	});
+  
+  it("docstring", () => {
+    const test = prepare({
+      name: "docstring",
+      content: "`test escaped?\\`\"\\\" new line? \\n`",
+      expect: "test escaped?`\"\" new line? \n"
+    });
+    return test();
+  });
+	
+	it("eval", () => {
+    const test = prepare({
+      name: "eval",
+      content: "123",
+      args: ["Number($0) + 321"],
+      expect: 444
+    });
+    return test();
+  });
+  
+  describe("indent", () => {
+    const ctx = {
+      sourceContent: "  $inline('foo|indent')",
+      inlineDirective: {
+        start: 2,
+        end: 23
+      }
+    };
+    const test = prepare({
+      name: "indent",
+      content: "foo\nbar",
+      ctx,
+      expect: "foo\n  bar"
+    });
+    
+    it("basic", () => test());
+    
+    it("no indent", () => test({
+      ctx: Object.assign({}, ctx, {sourceContent: "__$inline('foo|indent')"}),
+      expect: "foo\nbar"
+    }));
+  });
+	
+	it("parse", () => {
+    const test = prepare({
+      name: "parse",
+      content: '{"version": "1.2.3","nested": {"prop": 123}}'
+    });
+    return Promise.all([
+      test({args: ["version"], expect: "1.2.3"}),
+      test({args: ["nested", "prop"], expect: 123})
+    ]);
+	});
+  
+  it("string", () => {
+    const test = prepare({
+      name: "string",
+      content: Buffer.from("我")
+    });
+    return Promise.all([
+      test({expect: "我"}),
+      test({args: ["binary"], expect: 'æ'}),
+      test({content: "test", expect: "test"})
+    ]);
+  });
+  
+  it("stringify", () => {
+    const test = prepare({
+      name: "stringify",
+      content: "some text",
+      expect: '"some text"'
+    });
+    return test();
+  });
+  
+  it("trim", () => {
+    const test = prepare({
+      name: "trim",
+      content: " foo  ",
+      expect: "foo"
+    });
+    return test();
+  });
+	
+	it("markdown", () => {
+    const test = prepare({
+      name: "markdown",
+      content: "some text"
+    });
+    return Promise.all([
+      test({args: ["codeblock"], expect: "```\nsome text\n```"}),
+      test({args: ["code"], expect: "`some text`"}),
+      test({args: ["quote"], expect: "> some text"}),
+      test({
+        args: ["quote"],
+        content: "some text\nsome text",
+        expect: "> some text\n> some text"
+      }),
+      test({args: ["unknown"], error: true})
+    ]);
+	});
+	
+	describe("dataurl", () => {
+    const test = prepare({name: "dataurl"});
+    
+    it("unknown file, default to text file", () =>
+      test({
+        source: {name: "file", args: ["test"]},
+        content: Buffer.from("foo"),
+        expect: dataurl.format({data: Buffer.from("foo"), mimetype: "text/plain", charset: "utf8"})
+      })
+    );
+    
+    it("big5 encoded binary", () =>
+      test({
+        source: {name: "raw", args: ["test"]},
+        args: ["text/plain", "big5"],
+        content: Buffer.from("foo"),
+        expect: dataurl.format({data: Buffer.from("foo"), mimetype: "text/plain", charset: "big5"})
+      })
+    );
+    
+    it("text file", () =>
+      test({
+        source: {name: "file", args: ["test.css"]},
+        content: "foo",
+        expect: dataurl.format({data: "foo", mimetype: "text/css", charset: "utf8"})
+      })
+    );
+    
+    it("binary file", () =>
+      test({
+        source: {name: "file", args: ["test.png"]},
+        content: Buffer.from("foo"),
+        expect: dataurl.format({data: Buffer.from("foo"), mimetype: "image/png"})
+      })
+    );
+	});
+  
+  it("handle promise", () => {
+    transformer.add({
+      name: "testPromise",
+      transform() {
+        return new Promise(resolve => {
+          setTimeout(() => resolve("OK"), 10);
+        });
+      }
+    });
+    
+    const test = prepare({name: "testPromise", expect: "OK"});
+    return test();
+  });
+});
